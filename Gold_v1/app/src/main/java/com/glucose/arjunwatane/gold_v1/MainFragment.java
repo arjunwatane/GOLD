f312de1b9657bfb053ad5e3cab3076a3427a4137
arjunwatane <arjunwatane@knights.ucf.edu>
2016-10-07 21:26:42 -0400

    Basic Interface

---
+++ Gold_v1/app/src/main/java/com/glucose/arjunwatane/gold_v1/MainFragment.java
@@ -0,0 +1,34 @@
+package com.glucose.arjunwatane.gold_v1;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment
+{
+
+
+    public MainFragment()
+    {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+        /*TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView; */
+    }
+
+}
